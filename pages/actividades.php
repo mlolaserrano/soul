7d8fb6c5b63ba3ba73ca16569b7eaa13f2fe5ab2
Lara140620 <laol.tareas@gmail.com>
2024-08-21 11:45:01 -0300

    actividades footer php

--- pages/actividades.php
+++ pages/actividades.php
@@ -294,22 +294,7 @@
 </section>
        
       <footer>
-        <button id="chat"><img src="/images/Chat.png" width="150"></button>
-        <div id="chatVentana" class="chat-ventana hidden">
-            <div class="chat-header">
-                <span>Chat</span>
-                <button id="cerrar" class="cerrar">X</button>
-            </div>
-            <div class="chat-body" id="chatBody">
-                <p>¡Hola Juana! ¿En que te puedo ayudar?</p>
-            </div>
-            <div class="chat-footer">
-                <input type="text" id="mensajeInput" placeholder="Escribe un mensaje...">
-                <button id="enviar">Enviar</button>
-            </div>
-        </div>
-        <hr>
-        <p class="footer">&copy; Marina Duarte - Lara Olcesi - Lola Serrano </p>
+       <?php include "/componentes/footer.php"?>
     </footer>
 
  
