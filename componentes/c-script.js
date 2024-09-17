ee6ffa94b4e271988a4d2084108d529832cd2a7f
MarDuarte03 <marina.ailen.duarte@gmail.com>
2024-09-17 20:15:13 -0300

    componente manu(adm)

--- componentes/c-script.js
+++ componentes/c-script.js
@@ -108,34 +108,42 @@ class componenteAdm extends HTMLElement {
   connectedCallback() {
     this.innerHTML =
       `
-           <nav>
-       <ul class="navegacion">
-         <li class="logo">
-           <a href="../../pages/admin/(adm)home.html"><img src="../../images/Logo.png" alt="Logo de SOUL"></a>
-         </li>
-         <li class="busqueda">
-           <div class="search-container">
-             <input type="text" placeholder="Buscar..." class="buscador1">
-             <i class="fi fi-ts-search-heart" style="font-size: 25px;"></i>
-         </div>
-           </div>
-         </li>
-         <li class="pregunta"><i class="fi fi-tr-seal-question"></i></li>
-         <li class="usuario"><i class="fi fi-tr-man-head"></i></li>
-         <a href="/index.html" class="sin-formato">
-           <li class="salir"><i class="fi fi-ts-sign-out-alt"></i></li>
-         </a>
-       </ul>
-     </nav>
-     <div class="submenu">
-       <ul class="submenu-list">
-         <li><a href="(adm)calendario.html" class="bubbly-submenu">Calendario</a></li>
-         <li><a href="(adm)actividades.html" class="bubbly-submenu">Actividades</a></li>
-         <li><a href="(adm)acompañante.html" class="bubbly-submenu">Mi Acompañante</a></li>
-         <li><a href="(adm)recompensa.html" class="bubbly-submenu">Recompensas</a></li>
-       </ul>
-     </div>
-   </nav>
+    <nav>
+      <div class="menu">
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbox">
+          <i class="bi bi-list"></i>
+      </label>
+      <a href="../../pages/admin/(adm)home.html" class="enlace-home">
+          <img src="../../images/Logo.png" alt="" class="logo">
+      </a>
+      <ul class="iconos">
+          <li class="icon">
+              <div class="search-container">
+            <input type="text" placeholder="Buscar..." class="buscador1">
+            <i class="fi fi-ts-search-heart" style="font-size: 20px;"></i>
+          </li>
+          <li class="icon">
+              <i class="fi fi-tr-seal-question"></i>
+          </li>
+          <li class="icon">
+               <i class="fi fi-tr-man-head"></i>
+          </li>
+          <li class="icon">
+              <a href="../index.html">
+              <i class="fi fi-ts-sign-out-alt" id="salir"></i>
+              </a>
+          </li>
+      </ul>
+
+           <ul class="submenu-list">
+              <li><a href="../../pages/admin/(adm)calendario.html" class="bubbly-submenu">Calendario</a></li>
+              <li><a href="../../pages/admin/(adm)actividades.html" class="bubbly-submenu">Actividades</a></li>
+              <li><a href="../../pages/admin/(adm)acompañante.html" class="bubbly-submenu">Mi Acompañante</a></li>
+              <li><a href="../../pages/admin/(adm)recompensa.html" class="bubbly-submenu">Recompensas</a></li>
+          </ul>
+      </div>
+  </nav>
        `;
   }
 }
